84dbe10c41adc0ee37d080e9ade555bbd0243a8f
Alyona <poluektova@helper-studio.com>
2018-06-04 23:12:56 +0700

    add rest api controller support

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var Connection = require("./connection");
-
+var ControllerInitializer = require('./restApi/controllerInitializer');
 
 var connection = new Connection(http);
 
+
 http.listen(3000, function() {
+    var controllerInitializer = new ControllerInitializer(app);
+    controllerInitializer.initialize();
 
     console.log('listening on *:3000');
 });
